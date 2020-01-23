fbfcabe8f7130dc46dc350080c87e3de8349e809
Giri <hilmangp@gmail.com>
2020-01-23 20:05:43 +0700

    Updates

---
+++ precache-manifest.c74b703d78960bd15b54287b5b89d1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f8d0f8cd3f947025e8372cb6100977",
+    "url": "/monster-rolobox-react1/index.html"
+  },
+  {
+    "revision": "94fc4a58d7dbead696a5",
+    "url": "/monster-rolobox-react1/static/css/main.ef7113fd.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monster-rolobox-react1/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolobox-react1/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94fc4a58d7dbead696a5",
+    "url": "/monster-rolobox-react1/static/js/main.f803d348.chunk.js"
+  },
+  {
+    "revision": "2b8d76ee861f4f97831b",
+    "url": "/monster-rolobox-react1/static/js/runtime-main.97905183.js"
+  }
+]);
